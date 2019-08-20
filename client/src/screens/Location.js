be4b54a49f526eaa07f709a0f65a070f9abb4a4f
Franklin Mamvoula <alfranklino@gmail.com>
2019-08-20 17:39:09 -0400

    Chore: Map Showing and zoom works fine.

--- client/src/screens/Location.js
+++ client/src/screens/Location.js
@@ -1,22 +1,91 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, ScrollView, Text, StyleSheet } from "react-native";
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
 
-const Location = () =>{
+import gql from "graphql-tag";
+import { useQuery } from "@apollo/react-hooks";
 
-    return(
-        <View style={Styles.container}>
-            <Text>Location</Text>
-        </View>
-    )
+function getCoords() {
+  const { loading, error, data } = useQuery(gql`
+    {
+      getMapData(input: { token: 1, brewery_id: 1 }) {
+        latitude
+        longitude
+        latitudeDelta
+        longitudeDelta
+      }
+    }
+  `);
+
+  if (loading) return <Text>Loading...</Text>;
+  if (error) return <Text>Error:</Text>;
+  if (!data) {
+    return <Text>Loading...</Text>;
+  } else {
+    let mapData = data.getMapData;
+    // console.log(mapData);
+
+    return mapData;
+  }
+}
+
+function Location() {
+  const coords = getCoords();
+
+  console.log(coords);
+
+  return (
+    <ScrollView>
+      <Text style={Styles.textTitle}>Location</Text>
+      <View style={{ height: 300 }}>
+        <MapView
+          provider={PROVIDER_GOOGLE}
+          style={Styles.map}
+          initialRegion={{
+            latitude: 43.644,
+            longitude: -79.39993,
+            latitudeDelta: 0.001757,
+            longitudeDelta: 0.003866
+          }}
+          zoomEnabled={true}
+          zoomControlEnabled={true}
+        >
+          <Marker
+            coordinate={
+              (LatLng = {
+                latitude: 43.644,
+                longitude: -79.39993
+              })
+            }
+          />
+        </MapView>
+      </View>
+    </ScrollView>
+  );
 }
 
 const Styles = StyleSheet.create({
-    container:{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-   
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 120
+  },
+  textContent: {
+    margin: 20,
+
+    textAlign: "justify"
+  },
+  textTitle: {
+    textAlign: "center",
+    fontSize: 25,
+    fontWeight: "bold",
+    marginTop: 120,
+    marginBottom: 10
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject
+  }
 });
 
 export default Location;
