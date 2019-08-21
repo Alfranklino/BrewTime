990f448d7cfba8289d2336c7fd1f0a26f367b182
Franklin Mamvoula <alfranklino@gmail.com>
2019-08-21 01:03:48 -0400

    Feat: Initialization of the calendar.

--- client/src/screens/Booking.js
+++ client/src/screens/Booking.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, ScrollView, Text, StyleSheet, Button } from "react-native";
+import { Calendar } from "react-native-calendars";
 
 const Booking = props => {
   return (
-    <View style={Styles.container}>
-      <Text>Booking</Text>
+    <ScrollView>
+      <Text style={Styles.textTitle}>Booking</Text>
+      <View style={{ height: 350 }}>
+        <Calendar style={Styles.calendar} current={"2019-08-21"} />
+      </View>
       <Button
         title="View my Bookings"
         onPress={() => props.navigation.navigate("MyBookings")}
@@ -13,15 +17,50 @@ const Booking = props => {
         title="View This Booking Details"
         onPress={() => props.navigation.navigate("BookingDetails")}
       />
-    </View>
+    </ScrollView>
   );
 };
 
 const Styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: "center",
     justifyContent: "center",
-    alignItems: "center"
+    marginTop: 120
+  },
+  sectionAddresses: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 20
+  },
+  textContent: {
+    margin: 20,
+    textAlign: "justify"
+  },
+  textContent2: {
+    fontSize: 20,
+    fontWeight: "bold",
+    margin: 17,
+    color: "green",
+    textAlign: "justify"
+  },
+  textTitle: {
+    textAlign: "center",
+    fontSize: 32,
+    fontWeight: "bold",
+    marginTop: 30,
+    marginBottom: 10
+  },
+  textTitle2: {
+    textAlign: "center",
+    fontSize: 26,
+    fontWeight: "bold",
+    marginTop: 40,
+    marginBottom: 10
+  },
+  calendar: {
+    ...StyleSheet.absoluteFillObject
   }
 });
 
